468b431501583c2097ec6aba27e640e720ba9609
JessicaTiffanyD <66506723+JessicaTiffanyD@users.noreply.github.com>
2020-06-09 20:23:20 +0530

    Update and rename list,tuples and dictionaries to list,tuples and dictionaries.py
    #assigning elements to a different list
    #accessing element in a tuple
    #deleting an element in a dictionary

--- list,tuples and dictionaries.py
+++ list,tuples and dictionaries.py
@@ -1,4 +1,5 @@
 #assigning elements to a different list
+
 print("List:")
 list1=[1,2,3,4,5,6,7,8,9,10]
 list2=[]
@@ -10,12 +11,14 @@ print 'list1 = ', list1
 print 'list2 = ', list2
 
 #accessing element in a tuple
+
 print("Tuple:")
 tuple=('Hello','hi','bye')
 print(tuple[:3])
 print(tuple[2])
 
 #deleting an element in a dictionary
+
 print("Dictionary:")
 dict={1:"Apple", 2:"Ball", 3:"Cat", 4:"Dog", 5:"Elephant"}
 for i in dict.keys():
