90261f1bde30ff54875b7735bf01fce630f8864e
JessicaTiffanyD <66506723+JessicaTiffanyD@users.noreply.github.com>
2020-06-12 22:37:02 +0530

    To find the positive numbers in the list and putting it in a list.

---
+++ PositiveNumber.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+new_list1 = list(filter(lambda x: x >0, list1))
+print(new_list1)
+new_list2 = list(filter(lambda x: x >0, list2))
+print(new_list2)       
+
+
+
+      
